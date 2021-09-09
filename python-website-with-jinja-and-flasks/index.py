7d5906fdf090ebb9ce6d6a8b3c168fcfaf06ea4a
jamshed farzand <jamshed1981pk@gmail.com>
2021-09-09 21:49:15 +0500

    add index and services pages with python layout

---
+++ python-website-with-jinja-and-flasks/index.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return render_template('index.html')
+
+
+@app.route("/services")
+def services():
+    return render_template('services.html')
+
+
+app.run(debug=True)
